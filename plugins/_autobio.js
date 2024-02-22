d8747434f3b49fd493c23ad24d71b628aca18e41
JasonMomanyi <125371997+Lgufy@users.noreply.github.com>
2024-02-22 18:25:28 +0300

    Create _autobio.js

---
+++ plugins/_autobio.js
@@ -0,0 +1,17 @@
+import moment from 'moment-timezone';
+
+export async function updateBioAccordingToTime(conn) {
+    const nairobiTime = moment.tz(Date.now(), 'Africa/Nairobi').format('YYYY-MM-DD HH:mm:ss');
+    const bio = `Current Time: ${nairobiTime} JasonMomanyic2024`;
+    await conn.setStatus(bio);
+}
+
+export async function updateBioOnInterval(conn) {
+    // Update bio initially
+    await updateBioAccordingToTime(conn);
+
+    // Update bio every minute
+    setInterval(async () => {
+        await updateBioAccordingToTime(conn);
+    }, 60000); // 1 minute interval
+}
