dd903d5efa0ffc0b0b3d159bc95200303e12a7eb
JasonMomanyi <125371997+Lgufy@users.noreply.github.com>
2024-02-23 00:38:16 +0300

    Create main-Guru.js

---
+++ plugins/main-Guru.js
@@ -0,0 +1,33 @@
+let handler = async m => m.reply(`
+
+≡  *exterm-JX*   GROUPS
+
+─────────────
+▢ Join public bot group and support
+https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
+
+▢ Group 2
+https://chat.whatsapp.com/L6SQD9WKNavBmxWiXfcXCM
+
+▢ Group 3
+https://chat.whatsapp.com/L6SQD9WKNavBmxWiXfcXCM
+
+─────────────
+≡ Disabled links? enter here! 
+
+▢ Group WhatsApp 
+ https://chat.whatsapp.com/L6SQD9WKNavBmxWiXfcXCM
+─────────────
+▢ *Owner Telegram*
+ https://t.me/jasonmmomanyi
+
+▢ *YouTube*
+• https://www.youtube.com/JasonOrandi
+
+
+`.trim())
+handler.help = ['gpguru']
+handler.tags = ['main']
+handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru'] 
+
+export default handler
